b562654d03040038bfc67261b0e46b45b0b680d4
SeungHyeon Lee <diqzk1562@naver.com>
2023-06-06 16:17:27 +0900

    챗봇 성능 향상

--- PandaServer2.py
+++ PandaServer2.py
@@ -3,17 +3,41 @@ from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
 from flask import Flask, request,jsonify
 from tensorflow import keras
+from konlpy.tag import Okt
 import numpy as np
 import tensorflow
 import random
 import json
-import nltk
-from nltk.stem.lancaster import LancasterStemmer
-nltk.download('punkt')
-stemmer = LancasterStemmer()
+okt = Okt()
+
+################################################################
+# 챗봇 분석 전에 가져올  항목들
+with open('intents.json', 'rb') as file:
+    file_content = file.read().decode('utf-8')
+data = json.loads(file_content)
+
+# 챗봇 분석 전에 처리할 항목들
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds = okt.morphs(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+
+words = [okt.morphs(w.lower())[0] for w in words if w != "?"]
+words = sorted(list(set(words)))
+labels = sorted(labels)
+################################################################
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -95,32 +119,24 @@ def classification():
     }
     return jsonify(data)
 
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = okt.morphs(s)
+    s_words = [okt.morphs(word.lower())[0] for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return np.array(bag).reshape(1, -1)
+
 @app.route('/chatbot',methods=['GET','POST'])
 def chatbotData():
-    with open('intents.json', 'rb') as file:
-        file_content = file.read().decode('utf-8')
 
-    data = json.loads(file_content)
-
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
-
-    for intent in data['intents']:
-        for pattern in intent['patterns']:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent["tag"])
-
-        if intent['tag'] not in labels:
-            labels.append(intent['tag'])
-
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
-
-    labels = sorted(labels)
+    inp = request.get_data(as_text=True)
+    print('전달된 문자열:', inp)
 
     training = []
     output = []
@@ -130,8 +146,8 @@ def chatbotData():
     for x, doc in enumerate(docs_x):
         bag = []
 
-        wrds = [stemmer.stem(w.lower()) for w in doc]
-
+        wrds = [okt.morphs(w.lower())[0] for w in doc]
+        # print(wrds)
         for w in words:
             if w in wrds:
                 bag.append(1)
@@ -147,14 +163,11 @@ def chatbotData():
     tensorflow.compat.v1.reset_default_graph()
     model = keras.models.load_model("model.h5")
 
-    inp = request.get_data(as_text=True)
-    print('전달된 문자열:', inp)
-
     results = model.predict([bag_of_words(inp, words)])
 
     max_prob = max(results[0])
 
-    if max_prob < 0.5:
+    if max_prob < 0.45:
         return '이해가 가질 않습니다'
     else:
         results_index = np.argmax(results)
@@ -168,19 +181,6 @@ def chatbotData():
         print(message)
 
         return message
-
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
-
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
-
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
-
-    return np.array(bag).reshape(1, -1)
 
 if __name__ == '__main__':
     app.run()
